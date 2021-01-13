3f2620b95f1ab2de5919bcb8e332dd30680b4318
Maikon Gabriel <62940651+Maikongab20@users.noreply.github.com>
2021-01-12 22:25:29 -0300

    usando knex

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "knex_test",
+      user: "postgres",
+      password: "10111998"
+    },
+    migrations:{
+      tableName: 'knex_migrations',
+      directory:`${__dirname}/src/database/migrations`
+
+    },
+    seeds:{
+      directory:`${__dirname}/src/database/seed`
+    }
+  }
+
+};
