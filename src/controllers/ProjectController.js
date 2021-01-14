e7e5decec7e0d69263c8a83737856a65075e5d17
Maikon Gabriel <62940651+Maikongab20@users.noreply.github.com>
2021-01-14 20:40:14 -0300

    comfigurando o contttrolher do project

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -1,13 +1,51 @@
-const knex = require('../database')
+const knex = require('../database');
+const { create } = require('./UserController');
 
 
 module.exports = {
     async index(req, res, next){
        try {
-         const results = await knex('project')
+         const {user_id, page = 1} = req.query;
+
+
+         const query = knex('project')
+         .limit(5)
+         .offset((page - 1) * 5)
+
+         if(user_id){
+            query
+            .where({user_id})
+            .join('users','users.id','=','project.user_id')
+            .select('project.*', 'users.username')
+
+         }
+         const [count] = await knex('project').count()
+         console.log(count)
+
+         res.header('X-Total-Count',count["count"])
+
+         const results = await query
+
+
          return res.json(results)
        } catch (error) {
           next(error)
+       }
+   },
+   async create(req, res, next){
+      try {  
+         const {title, user_id} = req.body
+
+  
+         await knex('project').insert({
+           title,
+           user_id
+        })
+        
+        return res.status(201).send()
+      } catch (error) {
+         next(error)
+         
        }
    }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,12 @@ const UserController = require('./controllers/UserController')
 const ProjectController = require('./controllers/ProjectController')
 
 routes.get('/users', UserController.index)
-routes.post('/users', UserController.create)
-routes.put('/users/:id',UserController.update)
-routes.delete('/users/:id',UserController.delete)
+      .post('/users', UserController.create)
+      .put('/users/:id',UserController.update)
+      .delete('/users/:id',UserController.delete)
 
 // projectts
-routes.get('/project',ProjectController.index)
+      .get('/project',ProjectController.index)
+      .post('/project',ProjectController.create)
 
  module.exports = routes
